98db6d559cd4e92efe6a1c1e7607683cf60d0d98
Aaron Snowberger <jekkilekki@gmail.com>
2016-11-27 18:14:42 +0900

    Smooth scrolling with fixed bottom button

--- js/scroll-navbar.js
+++ js/scroll-navbar.js
@@ -19,26 +19,25 @@ $( document ).ready( function() {
       $(this).addClass( 'active' );
     });
 
-    var containers = $( '#hero .container, #get-started .container, #contact .container' );
+    /* Turn on active state on the correct link when scrolling */
+    /* @link:  http://codetheory.in/change-active-state-links-sticky-navigation-scroll/ */
+    var sections = $( '.section' ),
+        nav = $( 'nav, .view-more' ),
+        nav_height = nav.outerHeight();
+
+    var containers = $( '.full-height' );
     $(window).on("resize", function() {
       containers.each(function() {
-        var containerHeight = $(this).height();
+        var containerHeight = $(this).height() + nav_height;
         if( containerHeight >= $(window).height() ) {
-          $(this).css( "padding-top", "6em" );
+          // $(this).css( "padding-top", "6em" );
           $(this).css( "margin-bottom", "0" );
         } else {
-          $(this).css( "padding-top", ( $(window).height() - containerHeight - 73 ) / 2 + "px" );
-          $(this).css( "margin-bottom", ( $(window).height() - containerHeight - 73 ) / 2 + "px" );
+          $(this).css( "padding-top", ( $(window).height() - containerHeight ) / 2 + "px" );
+          $(this).css( "margin-bottom", ( $(window).height() - containerHeight ) / 2 + "px" );
         }
       });
     }).resize();
-
-
-    /* Turn on active state on the correct link when scrolling */
-    /* @link:  http://codetheory.in/change-active-state-links-sticky-navigation-scroll/ */
-    var sections = $( 'article' ),
-        nav = $( 'nav, .view-more' ),
-        nav_height = nav.outerHeight();
 
     // Try to get the nav-links to STOP being active on click
     // (currently they stay active until the screen has scrolled to the next section)
@@ -48,6 +47,11 @@ $( document ).ready( function() {
       else navlinkClicked = false;
     });
 
+    /**
+     * Active section manager
+     *
+     * Adds an 'active' class to any active nav-link in the main menu or any currently visible <article>
+     */
     $(window).on( 'scroll', function() {
        var cur_pos = $(this).scrollTop();
 
@@ -66,7 +70,7 @@ $( document ).ready( function() {
               nav.find( 'a' ).removeClass( 'active' );
               sections.removeClass( 'active' );
 
-              $('section').last().addClass( 'active' );
+              $('article').last().addClass( 'active' );
               $('.nav-link').last().addClass( 'active' );
           }
        });
@@ -74,7 +78,7 @@ $( document ).ready( function() {
 
     /* Smooth Scroll from CSS Tricks - specific to front page */
     /* @link: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
-    $('a[href*="#"]:not([href="#"]), .view-more').click(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname ) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -92,25 +96,49 @@ $( document ).ready( function() {
         }
     });
 
-    // Scroll arrow function - always scroll to next section if clicked, (back to top if clicked at last section - or display: none?)
-    // @TODO: Make this work
-    var firstSectionId = '#home';
-    var activeSection, activeSectionId = '#home';
-    var nextSection, nextSectionId = '#features';
-      $(window).bind( 'scroll', function() {
-        $( '.section' ).each(function() {
-          var art = $(this);
-          var pos = art.position().top - $(window).scrollTop();
+      /**
+       * Scroll down button
+       *
+       * When clicked, it checks the currently 'active' section and scrolls to the NEXT section
+       * If the 'active' section is the LAST section, it scrolls back to the top
+       */
+       var activeSection, activeSectionId = 'home';
+         $(window).bind( 'scroll', function() {
+           $( '.section' ).each(function() {
+             var art = $(this);
+             var pos = art.position().top - $(window).scrollTop();
 
-          if( pos <= 0 && art.hasClass( 'active' ) ) {
-            activeSection = art;
-            activeSectionId = '#' + activeSection.attr( 'id' );
+             if( art.hasClass( 'active' ) ) {
+               activeSection = art;
+               activeSectionId = activeSection.attr( 'id' );
+             }
+           });
+         });
 
-            nextSection = art.nextAll( 'article' );
-            nextSectionId = '#' + nextSection.attr( 'id' );
+       var articleIds = [ 'home' ];
+       var articles = $( 'article' ).each( function() {
+         articleIds.push( this.id );
+       });
+       var firstSectionId = 'home';
+
+      $( '.view-more' ).click( function() {
+        // As long as this is not the LAST element in the articleIds array (end of the page)
+        if ( activeSectionId !== articleIds[ articleIds.length - 1 ] ) {
+          var target = $('#' + articleIds[ $.inArray( activeSectionId, articleIds ) + 1 ] );
+            $('html,body').animate({
+                scrollTop: target.offset().top - nav_height + 1 // add 1px to be sure we cross INTO the next section
+            }, 1000);
+            return false;
+        } else {
+          var target = $('#home');
+            $('html,body').animate({
+                scrollTop: 0
+            }, 1000);
+            activeSectionId = 'home';
+            return false;
           }
-        });
       });
+
 
     // nav.find('a').on('click', function () {
     //   var $el = $(this)
